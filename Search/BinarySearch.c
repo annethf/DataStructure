3d7ee04f9d8313466ba72d81e99e88dd3b06dee3
annethf <annethf@outlook.com>
2015-05-25 17:02:01 +0800

    Search

--- Search/BinarySearch.c
+++ Search/BinarySearch.c
@@ -7,27 +7,13 @@ int binSearch(seqList slist, keyType key)
 		mid = (low + high) / 2;
 		if(slist[mid].key == key) return mid;
 		if(slist[mid].key < key)
-			high = mid + 1;
+			low = mid + 1;
 		else
-			low = mid - 1;
+			high = mid - 1;
 	}
 	return 0;  //low < high 查找失败！
 }
-//升序排列
-int binSearch(seqList slist, keyType key)
-{
-	int low = 1, high = slist.length(), mid = 0;
-	while(low <= high)
-	{
-		mid = (low + high) / 2;
-		if(slist[mid].key == key) return mid;
-		if(slist[mid].key > key)
-			high = mid - 1;
-		else
-			low = mid + 1;
-	}
-	return 0;
-}
+
 //降序排列
 int binSearch(seqList slist, keyType key)
 {
@@ -36,30 +22,18 @@ int binSearch(seqList slist, keyType key)
 	{
 		mid = (low + high) / 2;
 		if(slist[mid].key == key) return mid;
-		if(slist[mid].key < key)
-			high = mid - 1;
-		else
-			low = mid + 1;
-	}
-	return 0;
-}
-
-int binSearch(seqList slist, keyType key)
-{
-	int low = 1, high = slist.length(), mid = 0;
-	while(low <= high)
-	{
-		mid = (low + high) / 2;
-		if(slist[mid].key == key) return mid;
 		if(slist[mid].key > key)
-			high = mid + 1;
+			low = mid + 1;
 		else
-			low = mid - 1;
+			high = mid - 1;
 	}
 	return 0;
 }
 
-
+/*
+无论是升序还是降序，都要调整判断条件slist[mid].key与key的比较符号，保证high=mid-1;
+high一定不能是mid+1，因为这样会进入四循环
+*/
 
 
 
